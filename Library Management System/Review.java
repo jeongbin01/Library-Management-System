bc118621483e3b4db3b5accaf10d17e6dff1e926
Jeong Bin <149468935+jeongbin01@users.noreply.github.com>
2024-07-30 10:55:56 +0900

    Update Review.java

--- Library Management System/Review.java
+++ Library Management System/Review.java
@@ -1,31 +1,23 @@
 public class Review {
     private long id;
-    private long bookId;
-    private long memberId;
-    private String content;
+    private String comment;
     private int rating;
 
-    public Review(long bookId, long memberId, String content, int rating) {
+    public Review(String comment, int rating) {
         this.id = IDGenerator.generateID();
-        this.bookId = bookId;
-        this.memberId = memberId;
-        this.content = content;
+        this.comment = comment;
         this.rating = rating;
     }
 
     public long getId() { return id; }
-    public long getBookId() { return bookId; }
-    public long getMemberId() { return memberId; }
-    public String getContent() { return content; }
+    public String getComment() { return comment; }
     public int getRating() { return rating; }
 
     @Override
     public String toString() {
         return "Review{" +
                 "id=" + id +
-                ", bookId=" + bookId +
-                ", memberId=" + memberId +
-                ", content='" + content + '\'' +
+                ", comment='" + comment + '\'' +
                 ", rating=" + rating +
                 '}';
     }
